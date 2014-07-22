0763eed81e0e08da167ea4decf5967df3d057e53
vito-dante <vitoallstarz@gmail.com>
2014-07-22 10:50:50 -0400

    modificacion

--- sql/query contar el numero de tablas.sql
+++ sql/query contar el numero de tablas.sql
@@ -1,13 +1,14 @@
 //Para contar el numero de tablas que tienes en tu BD sirve para postgres y mysql
 
-SELECT Count(*) FROM information_schema.tables
 select count(*) from information_schema.tables where table_schema = 'information_schema';
 
+SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='YOUR_DB_NAME_HERE'
+
 SELECT tablename FROM pg_tables WHERE schemaname = 'public'
+
 SELECT count(*) FROM pg_tables WHERE schemaname = 'public'
 
 Para importar a postgresql //probado en postgresql for windows
 \i /path/to/yourfile.sql
 
 
-SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='YOUR_DB_NAME_HERE'
